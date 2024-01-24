cca1450639e3b580a051d81e3fdce246dbd3ec94
obaraelijah <elijahobara357@gmail.com>
2024-01-24 10:50:47 +0300

    feat: password entry fucntions e.g prompt service info etc

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
 mod pentry;
 
+use crate::pentry::prompt;
+use crate::pentry::read_passwords_from_file;
+use crate::pentry::ServiceInfo;
 
 fn clr() {
     print!("{}[2J", 27 as char);
@@ -22,5 +25,63 @@ fn main() {
         println!("2. List Entries");
         println!("3. Search");
         println!("4. Quit");
+
+        let mut choice = String::new();
+        std::io::stdin().read_line(&mut choice).unwrap();
+
+        match choice.trim() {
+           "1" => {
+               clr();
+               let entry = ServiceInfo::new(
+                    prompt("Service :"),
+                    prompt("Username :"),
+                    prompt("Password"),
+               );
+               println!("Entry added successfully.");
+               entry.write_to_file();
+           } 
+           "2" => {
+                clr();
+                let services = read_passwords_from_file().unwrap_or_else(|err| {
+                    eprintln!("Error reading passwords: {}", err);
+                    Vec::new();
+                });
+                for item in &services {
+                    println!(
+                        "service = {}
+                        - Username: {}
+                        - Password: {}",
+                        item.service, item.username, item.password
+                    
+                    );
+                }
+           }
+           "3" => {
+                clr();
+                let services = read_passwords_from_file().unwrap_or_else(|err| {
+                    eprintln!("Error reading passwords: {}", err);
+                    Vec::new();
+                });
+                let search = prompt("search :");
+                for item in &services{
+                    if item.service.as_str() == search.as_str() {
+                        println!(
+                            "service = {}
+                            - Username: {}
+                            - Password: {}",
+                            item.service, item.username, item.password
+                        
+                        ); 
+                    }
+                }
+           } 
+           "4" => {
+                clr();
+                println!("Goodbye!");
+                break;
+           }
+           _ => println!("Invalid choice")
+        }
+        println!("\n\n");
     } 
 }
